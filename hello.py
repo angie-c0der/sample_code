7f489f21245ac18a7a8eb898849697d8ead695d2
angie-c0der <ar.csa2024@saintgits.org>
2022-12-17 18:40:33 +0530

    second commit

--- hello.py
+++ hello.py
@@ -4,8 +4,19 @@
     # print(line)
     # print("hi")
 
-file1 = open('ChatAnaghaAnne.txt', encoding='utf8')
+file1 = open('chat_2.txt', encoding='utf8')
 count = 0
+line1 = file1.readline()
 for line in file1:
-    message = line.split(',')[0]
-    time = date[1]    
+    date_list = line.split(',')
+    # print(date_list,len(date_list))
+    
+    date = date_list[0]
+    # print(date)
+    
+    time_list = date_list[1].split('-')
+    time = time_list[0]
+    contact_text = time_list[1].split(':')
+    contact = contact_text[0]
+    message = contact_text[1]    
+    print(date, time, contact, message)
